9a27956a0f0ecdd26631de583a69366da746e4ba
unknown <zf3dai@uwaterloo.ca>
2020-05-07 13:08:28 -0600

    working multiple jpeg buffers

--- firmware/components/camera_module/include/camera_module.h
+++ firmware/components/camera_module/include/camera_module.h
@@ -12,12 +12,14 @@
 
 #define CAMERA_MODULE_BASE		40
 
-#define CAMERA_TASK_PRIO		5
+#define CAMERA_TASK_PRIO		7
 
 extern TaskHandle_t camera_task;
 
 esp_err_t camera_module_init();
 
+esp_err_t camera_get_jpeg(void** buf_adr, uint32_t* size, TickType_t xTicksToWait);
 
+esp_err_t camera_return_jpeg(void *buf_adr);
 
 #endif
